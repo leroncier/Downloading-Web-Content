1916f96126c3d79b81c3fe37821050abbcc9a4f8
le roncier <leroncier@gmail.com>
2016-03-07 20:53:10 +0100

    Initial Commit

---
+++ Downloading Web Content/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Downloading Web Content
+//
+//  Created by Charles Roncier on 07/03/2016.
+//  Copyright © 2016 Charles Roncier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
