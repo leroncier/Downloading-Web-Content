ae4589351c3c060f4f22659f7b85f1df6c0b77f3
le roncier <leroncier@gmail.com>
2016-03-08 17:50:57 +0100

    iOS Courses : Web Content
    How to download content from the web

--- Downloading Web Content/ViewController.swift
+++ Downloading Web Content/ViewController.swift
@@ -13,6 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        let url = NSURL(string: "http://www.vih.org")!
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, ErrorType) -> Void in
+            // will happen when task is completes
+            if let urlContent = data {
+                print(urlContent)
+            } else {
+                //error message
+            }
+            
+        }
+        task.resume()
+    
     }
 
     override func didReceiveMemoryWarning() {
